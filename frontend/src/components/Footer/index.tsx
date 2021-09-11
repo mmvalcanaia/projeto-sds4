02d4de448bff787a3ebd30b628f7f249c2889609
Marina Merini Valcanaia <mmvalcanaia@gmail.com>
2021-09-11 13:14:26 +0200

    Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-        <div><footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/mmvalcanaia" target="_blank" rel="noreferrer">Marina Merini Valcanaia</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ALCM-bit" target="_blank" rel="noreferrer">Felipe Augusto Lopes de Carvalho Magalhães</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
-        </footer></div>
+        </footer>
     );
 }
 
